56eddff7f1d3634cfcb9ec576f98542071cf5b88
phamnhungoctuan <phamnhungoctuan@gmail.com>
2024-10-05 11:12:31 +0700

    Update story.sh

--- story.sh
+++ story.sh
@@ -3,42 +3,134 @@ set -euo pipefail
 
 # Functions
 
-mainMenu ()
-{
-    echo -e "\033[36m""Story Validator Tools""\e[0m"
-    echo "1 Install Story Node"
-    echo "2 Update Story Consensus"
-    echo "3 Update Story Geth"
-    echo "4 Create validator"
-    echo "5 Get latest block height"
-    echo "6 Get Validator dashboard link"
-    echo "7 Get Validator Public and Private Key"
-    echo "q Quit"
+download_and_extract() {
+    local url=$1
+    local output=$2
+    if [ -f "$output" ]; then
+        echo "$output already exists. Skipping download."
+    else
+        wget -qO "$output" "$url"
+        tar xf "$output"
+        cp story*/story /usr/local/bin
+        rm -rf story*/ "$output"
+    fi
 }
 
+check_command_success() {
+    if [ $? -ne 0 ]; then
+        echo "Error: $1 failed. Exiting."
+        exit 1
+    fi
+}
+
+mainMenu() {
+    echo -e "\033[36m""Story Validator Tools""\e[0m"
+    echo "1. Install Story Node"
+    echo "2. Update Story Consensus"
+    echo "3. Update Story Geth"
+    echo "4. Create validator"
+    echo "5. Get latest block height"
+    echo "6. Get Validator dashboard link"
+    echo "7. Get Validator Public and Private Key"
+    echo "8. Get faucet"
+    echo "q. Quit"
+}
+
+installStoryConsensus() {
+    echo "Getting Story Consensus..."
+    download_and_extract $(curl -s https://api.github.com/repos/piplabs/story/releases/latest | grep 'body' | grep -Eo 'https?://[^ ]+story-linux-amd64[^ ]+' | sed 's/......$//') story.tar.gz
+    check_command_success "Story Consensus download"
+}
+
+installStoryGeth() {
+    echo "Getting Story Geth..."
+    download_and_extract $(curl -s https://api.github.com/repos/piplabs/story-geth/releases/latest | grep 'body' | grep -Eo 'https?://[^ ]+geth-linux-amd64[^ ]+' | sed 's/......$//') story-geth.tar.gz
+    check_command_success "Story Geth download"
+}
+
+freshInstallSecondPart() {
+    installStoryGeth
+    read -p "Please enter your moniker: " moniker
+    story init --network iliad --moniker "$moniker"
+    createStoryConsensusServiceFile
+    createStoryGethServiceFile
+    echo "Adding Peers"
+    sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$(curl -sS https://story-rpc.oreonserv.com/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)\"/" "$HOME/.story/story/config/config.toml"
+    echo "Restarting the services"
+    sudo systemctl restart story story-geth
+}
+
+createServiceFile() {
+    local service_name=$1
+    local exec_command=$2
+
+    sudo tee "/etc/systemd/system/${service_name}.service" > /dev/null <<EOF
+    [Unit]
+    Description=${service_name} Service
+    After=network.target
+
+    [Service]
+    User=root
+    ExecStart=$exec_command
+    Restart=on-failure
+    RestartSec=3
+    LimitNOFILE=4096
+
+    [Install]
+    WantedBy=multi-user.target
+EOF
+    sudo systemctl daemon-reload
+    sudo systemctl enable "$service_name"
+    sudo systemctl start "$service_name"
+}
+
+createStoryConsensusServiceFile() {
+    createServiceFile "story" "/usr/local/bin/story run"
+}
+
+createStoryGethServiceFile() {
+    createServiceFile "story-geth" "/usr/local/bin/geth --iliad --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port 8545 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port 8546"
+}
+
+updateComponent() {
+    local component=$1
+    local service_name=$2
+    local install_function=$3
+
+    latest_version=$(curl -s https://api.github.com/repos/piplabs/$component/releases/latest | grep tag_name | cut -d\" -f4)
+    echo "Latest $component version is: $latest_version"
+    installed_version=$($component -v)
+    echo "Installed $component version: $installed_version"
+
+    read -p "Are you sure you want to update? (y/n) " yn
+    case $yn in
+        [yY])
+            echo "Updating $component..."
+            sudo systemctl stop "$service_name"
+            rm "/usr/local/bin/$component"
+            $install_function
+            sudo systemctl start "$service_name"
+            echo "Done updating $component!"
+            ;;
+        *)
+            echo "Exiting update process for $component."
+            ;;
+    esac
+}
 
 installMenu() {
     latestConsVersion=$(curl -s https://api.github.com/repos/piplabs/story/releases/latest | grep tag_name | cut -d\" -f4)
     secondChoiceVersion=$(curl -s "https://api.github.com/repos/piplabs/story/tags" | grep -oP '"name": "\K[^"]+' | sed -n '2p')
+
     PS3="Select an option: "
     select subopt in "Install version $latestConsVersion - Latest" "Install version $secondChoiceVersion" "Back"; do
         case $subopt in
             "Install version $latestConsVersion - Latest")
-                echo "Getting Story Consesus..."
-                wget -qO story.tar.gz $(curl -s https://api.github.com/repos/piplabs/story/releases/latest | grep 'body' | grep -Eo 'https?://[^ ]+story-linux-amd64[^ ]+' | sed 's/......$//')
-                echo "Extracting and configuring Story..."
-                tar xf story.tar.gz
-                cp story*/story /bin
-                rm -rf story*/ | rm story.tar.gz
+                installStoryConsensus
                 freshInstallSecondPart
                 ;;
             "Install version $secondChoiceVersion")
-                echo "Getting Story Consesus..."
-                wget -qO story.tar.gz $(curl -s https://github.com/piplabs/story/releases/tag/$secondChoiceVersion | grep binaries | grep -Eo 'https?://[^ ]+story-linux-amd64[^ ]+' | sed 's/.$//')
-                echo "Extracting and configuring Story..."
-                tar xf story.tar.gz
-                cp story*/story /bin
-                rm -rf story*/ | rm story.tar.gz
+                installStoryConsensus
                 freshInstallSecondPart
                 ;;
             "Back")
@@ -51,173 +143,48 @@ installMenu() {
     done
 }
 
-installStoryGeth () # Story Geth install function
-{
-    echo "Getting Story Geth..."
-    wget -qO story-geth.tar.gz $(curl -s https://api.github.com/repos/piplabs/story-geth/releases/latest | grep 'body' | grep -Eo 'https?://[^ ]+geth-linux-amd64[^ ]+' | sed 's/......$//')
-    echo "Extracting and configuring Story Geth..."
-    tar xf story-geth.tar.gz
-    cp geth*/geth /bin
-    rm -rf geth*/ | rm story-geth.tar.gz
-}
-
-installStoryConsensus () # Story Consensus install function
-{      
-    echo "Getting Story Consesus..."
-    wget -qO story.tar.gz $(curl -s https://api.github.com/repos/piplabs/story/releases/latest | grep 'body' | grep -Eo 'https?://[^ ]+story-linux-amd64[^ ]+' | sed 's/......$//')
-    echo "Extracting and configuring Story..."
-    tar xf story.tar.gz
-    cp story*/story /bin
-    rm -rf story*/ | rm story.tar.gz
-}
-
-freshInstallSecondPart ()
-{
-    installStoryGeth
-    echo "Please enter your moniker"
-    read varname
-    story init --network iliad --moniker $varname
-    createStoryConsensusServiceFile
-    createStoryGethServiceFile
-    echo "Adding Peers"
-    sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$(curl -sS https://story-rpc.oreonserv.com/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)\"/" $HOME/.story/story/config/config.toml
-    echo "Restarting the services"
-    sudo systemctl restart story story-geth
-}
-
-createStoryConsensusServiceFile ()
-{
-    echo "creating Stoy Consensus Service File"
-    sudo tee /etc/systemd/system/story.service > /dev/null <<EOF
-        [Unit]
-        Description=Story Consensus Client
-        After=network.target
-
-        [Service]
-        User=root
-        ExecStart=/bin/story run
-        Restart=on-failure
-        RestartSec=3
-        LimitNOFILE=4096
-
-        [Install]
-        WantedBy=multi-user.target
-EOF
-    echo "Starting Story Consensus Service"
-    sudo systemctl daemon-reload && \
-    sudo systemctl start story && \
-    sudo systemctl enable story
-}
-
-createStoryGethServiceFile ()
-{
-    echo "creating Stoy Geth Service File"
-    sudo tee /etc/systemd/system/story-geth.service > /dev/null <<EOF
-        [Unit]
-        Description=Story Geth Client
-        After=network.target
-
-        [Service]
-        User=root
-        ExecStart=/bin/geth --iliad --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 0.0.0.0 --http.port 8545 --ws --ws.api eth,web3,net,txpool --ws.addr 0.0.0.0 --ws.port 8546
-        Restart=on-failure
-        RestartSec=3
-        LimitNOFILE=4096
-
-        [Install]
-        WantedBy=multi-user.target
-EOF
-    echo "Starting Story Geth Service"
-    sudo systemctl daemon-reload && \
-    sudo systemctl start story-geth && \
-    sudo systemctl enable story-geth
-}
-
-
-while [[ 1 ]]
-do
+while true; do
     echo
     mainMenu
     echo
     read -ep "Enter the number of the option you want: " CHOICE
     echo
+
     case "$CHOICE" in
         "1") # Install Story node
             installMenu
-            echo
             ;;
         "2") # Update Story Consensus
-            latestStoryVersion=$(curl -s https://api.github.com/repos/piplabs/story/releases/latest | grep tag_name | cut -d\" -f4)
-            story version &> ver.txt
-            installedStoryVersion=$(grep Version ver.txt | awk '{print $2}' && rm ver.txt)
-            echo "Latest Stoy version is: $latestStoryVersion"
-            echo "Installed Story version: $installedStoryVersion"
-            while true; do
-            read -p "Are you sure want to update? (y/n) " yn
-            case $yn in 
-                [yY] ) echo updating...;
-                        echo "stopping the services"
-                        sudo systemctl stop story
-                        rm /bin/story
-                        installStoryConsensus
-                        echo "Starting the services"
-                        sudo systemctl start story
-                        echo "Done!"
-                        break;;
-                [nN] ) echo exiting...;
-                    exit;;
-                * ) echo invalid response;;
-            esac
-            done
-            echo
+            updateComponent "story" "story" installStoryConsensus
             ;;
         "3") # Update Story Geth
-            latestGethVersion=$(curl -s https://api.github.com/repos/piplabs/story-geth/releases/latest | grep tag_name | cut -d\" -f4)
-            installedGethVersion=$(geth -v)
-            echo "Latest Geth version is: $latestGethVersion"
-            echo "Installed Geth version: $installedGethVersion"
-            while true; do
-            read -p "Are you sure want to update? (y/n) " yn
-            case $yn in 
-                [yY] ) echo Updating...;
-                    echo "stopping the services"
-                    sudo systemctl stop story-geth
-                    rm /bin/geth
-                    installStoryGeth
-                    echo "Starting the services"
-                    sudo systemctl start story-geth
-                    echo "Done!"
-                    break;;
-                [nN] ) echo exiting...;
-                    exit;;
-                * ) echo invalid response;;
-            esac
-            done
-            echo
+            updateComponent "geth" "story-geth" installStoryGeth
             ;;
         "4") # Create the validator
-            echo "this will stake 0.5 IP to your validator, make sure you have some in your wallet"
-            echo "please enter your private key"
-            read -s key
-            story validator create --stake 500000000000000000 --private-key $key
+            echo "This will stake 0.5 IP to your validator, make sure you have some in your wallet."
+            read -s -p "Please enter your private key: " key
             echo
+            story validator create --stake 500000000000000000 --private-key "$key"
             ;;
         "5") # Get latest block height
             curl -s localhost:26657/status | jq .result.sync_info.latest_block_height
-            echo
             ;;
         "6") # Get Validator dashboard link
             address=$(cat ~/.story/story/config/priv_validator_key.json | grep address | cut -d\" -f4)
             echo "https://testnet.story.explorers.guru/validator/$address"
-            echo
-            ;;    
-        "7") # Get Validator Private Key
-            story validator export --export-evm-key
-            cat $HOME/.story/story/config/private_key.txt
-            echo
             ;;
-        "q") # quit the script entirely
+        "7") # Get Validator Public and Private Key
+            story validator export --export-evm-key
+            cat "$HOME/.story/story/config/private_key.txt"
+            ;;
+        "8") # Get faucet
+            echo "https://story.faucetme.pro/"
+            ;;
+        "q") # quit the script
             exit
+            ;;
+        *)
+            echo "Invalid option"
             ;;
     esac
 done
